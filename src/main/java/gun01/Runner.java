4dd4167c448316fd44a6fc9dccd70375f0142c12
atezgider <atezgider@gmail.com>
2023-03-28 22:40:54 +0200

    Initial

--- src/main/java/gun01/Runner.java
+++ src/main/java/gun01/Runner.java
@@ -1,10 +1,11 @@
 package gun01;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = {""},
         glue = {""}
 )
-public class Runner {
+public class Runner extends AbstractTestNGCucumberTests {
 }
