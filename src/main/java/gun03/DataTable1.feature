1add8323262a2fa40dcdb73183165c88e514c26f
atezgider <atezgider@gmail.com>
2023-03-30 20:22:03 +0200

    2023-03-30

---
+++ src/main/java/gun03/DataTable1.feature
@@ -0,0 +1,36 @@
+Feature: Data table samples
+
+
+  # List<String>
+  Scenario: sablon 1, list
+    Given my list 10
+      | 1 |
+      | 2 |
+      | 3 |
+      | 4 |
+      | 5 |
+      | 5 |
+    Then conclution
+
+
+
+
+      # List<List<String>>
+  Scenario: sablon 2, lists
+    Given my lists, list of list
+      | 1 | 11 | 21 | 31 | 41 |
+      | 2 | 12 | 22 | 32 | 42 |
+      | 3 | 13 | 23 | 33 | 43 |
+      | 4 | 14 | 24 | 34 | 44 |
+      | 5 | 15 | 25 | 35 | 45 |
+      | 5 | 15 | 25 | 35 | 45 |
+
+
+
+    # Map<String, String>
+  Scenario: sablon 3, map
+    Given my map
+      | key1 | 1  |
+      | key2 | 6  |
+      | key3 | 21 |
+      | key4 | 23 |
---
+++ src/main/java/gun03/StepdefsGun03.java
@@ -0,0 +1,47 @@
+package gun03;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import javax.xml.crypto.Data;
+import java.util.List;
+import java.util.Map;
+
+public class StepdefsGun03 {
+
+
+    @Given("my list {int}")
+    public void myList(int num, DataTable dataTable) {
+        List<Integer> list = dataTable.asList(Integer.class);
+
+        System.out.println(num);
+        System.out.println(list);
+
+        for (Integer s : list) {
+            System.out.println(s + " : " + (s*s));
+        }
+
+    }
+
+    @Then("conclution")
+    public void conclution() {
+    }
+
+    @Given("my lists, list of list")
+    public void myListsListOfList(DataTable table) {
+        List<List<Integer>> lists = table.asLists(Integer.class);
+        for (List<Integer> list : lists) {
+            System.out.println(list);
+        }
+    }
+
+
+    @Given("my map")
+    public void myMap(DataTable table) {
+        Map<String, Integer> map = table.asMap(String.class, Integer.class);
+
+        System.out.println(map);
+        System.out.println(map.get("key1"));
+    }
+}
